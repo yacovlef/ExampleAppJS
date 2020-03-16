f7f69a9567ac9b19c8ec797733619c80dcd11cf4
Yacovlef Alexey <yacovlef@gmail.com>
2020-03-16 20:46:55 +0300

    update component user form delete

--- frontend/src/store/root-reducer.ts
+++ frontend/src/store/root-reducer.ts
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import modalReducer from '../components/@common/Modal/modal.reducer';
 import userListReducer from '../components/User/UserList/user-list.reducer';
 import userFormReaducer from '../components/User/UserForm/user-form.reducer';
+import userFormDeleteReducer from '../components/User/UserFormDelete/user-form-delete.reducer'
 
 export default combineReducers({
     modal: modalReducer,
     userList: userListReducer,
-    userForm: userFormReaducer
+    userForm: userFormReaducer,
+    userFormDelete: userFormDeleteReducer
 });
