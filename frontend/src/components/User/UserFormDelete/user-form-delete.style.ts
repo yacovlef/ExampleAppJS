ce76f055296c138de357f79c3d05af21ea06562b
Yacovlef Alexey <yacovlef@gmail.com>
2020-03-30 10:25:49 +0300

    update component user form delete

--- frontend/src/components/User/UserFormDelete/user-form-delete.style.ts
+++ frontend/src/components/User/UserFormDelete/user-form-delete.style.ts
@@ -3,13 +3,11 @@ import styled from 'styled-components';
 const UserFormDeleteForm = styled.form``;
 
 const UserFormDeleteRow = styled.div`
-    padding: 0.5rem 0;
+    padding: 1rem 0;
     text-align: center;
 `;
 
-const UserFormDeleteTitle = styled.span`
-    font-size: 1.5rem;
-`;
+const UserFormDeleteTitle = styled.span``;
 
 export {
     UserFormDeleteForm,
