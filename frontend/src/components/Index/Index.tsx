ababd812ed620bbbe6123923c8b4b42f69ac8e96
Yacovlef Alexey <yacovlef@gmail.com>
2020-03-31 09:31:13 +0300

    update component index

--- frontend/src/components/Index/Index.tsx
+++ frontend/src/components/Index/Index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { IndexLayout } from './index.style';
+import { IndexLayout, IndexTitle } from './index.style';
 
 const Index = () => {
     return (
         <IndexLayout>
-            <h1>ExampleAppJS</h1>
+            <IndexTitle>ExampleAppJS</IndexTitle>
         </IndexLayout>
     );
 };
--- frontend/src/components/Index/index.style.ts
+++ frontend/src/components/Index/index.style.ts
@@ -2,8 +2,15 @@ import styled from 'styled-components';
 
 const IndexLayout = styled.div`
     text-align: center;
+
+    padding: 1rem;
+`;
+
+const IndexTitle = styled.span`
+    font-size: 2rem;
 `;
 
 export {
-    IndexLayout
+    IndexLayout,
+    IndexTitle
 };
